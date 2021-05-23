238352a8992b1f04df8743ba0ef0ba7d1e0bc1c4
curtistkd123 <curtislewis1989@yahoo.com>
2021-05-23 13:57:31 -0400

    starter

---
+++ YourRhythym-Blues/src/main/java/com/example/demo/YourRhythymBluesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourRhythymBluesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourRhythymBluesApplication.class, args);
+	}
+
+}
