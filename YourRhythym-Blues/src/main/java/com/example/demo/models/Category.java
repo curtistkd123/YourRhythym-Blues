41ba5fac11be8e9b340e1405cadcb061424d3803
User20 <revdavecarlton@gmail.com>
2021-05-24 20:32:25 -0500

    category model

--- YourRhythym-Blues/src/main/java/com/example/demo/models/Category.java
+++ YourRhythym-Blues/src/main/java/com/example/demo/models/Category.java
@@ -13,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
